13738b1d8efbf1fb6e158989dcf9e7f28dfb281e
milafilip <milafilipovwork1@gmail.com>
2019-03-10 15:57:43 -0400

    updated to use async

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ const Terminal = require('./lib/terminal')
 const { stdin, stdout } = process
 const terminal = new Terminal({ stdin, stdout })
 
-new Promise((resolve, reject) => {
-  if (process.env.CHANNEL) {
-    resolve(process.env.CHANNEL)
-  } else {
-    terminal.question('What channel do you want to join? ', (channelname) => {
-      resolve(channelname)
-    })
+async function getChannelname () {
+  let channelname = process.env.CHANNEL
+  if (!channelname || channelname.trim() === '') {
+    channelname = await terminal.question('What channel do you want to join? ')
   }
-}).then(channelname => {
+  return channelname
+}
+
+(async () => {
+  const channelname = await getChannelname()
   const username = process.env.USERNAME
   const password = process.env.TOKEN
-  new Chat({ channelname, username, password, terminal }).start()
-}).catch(e => {
+  const chat = new Chat({ channelname, username, password, terminal })
+  chat.start()
+})().catch(e => {
   console.error(e)
   process.exit(1)
 })

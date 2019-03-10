f19fcb7f8298b675702d02f3cac330f3678e4898
milafilip <milafilipovwork1@gmail.com>
2019-03-10 03:11:32 -0400

    refactored terminal and chat to use classes and added jest dep

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 require('dotenv').config()
-const chat = require('./lib/chat')
-const terminal = require('./lib/terminal')
-const color = require('./lib/color')
-const safeColors = require('colors/safe')
+const Chat = require('./lib/chat')
+const Terminal = require('./lib/terminal')
 
-const start = (channelname) => {
-  chat.start(channelname, process.env.USERNAME, process.env.TOKEN)
-}
+const { stdin, stdout } = process
+const terminal = new Terminal({ stdin, stdout })
 
-chat.onMessage((channelname, context, msg, self) => {
-  if (self) { return }
-  const name = context['display-name']
-  const newColor = color.getTerminalColor(context['color'])
-  const colorName = safeColors[newColor](name)
-  terminal.log(`${colorName}: ${msg}`)
+new Promise((resolve, reject) => {
+  if (process.env.CHANNEL) {
+    resolve(process.env.CHANNEL)
+  } else {
+    terminal.question('What channel do you want to join? ', (channelname) => {
+      resolve(channelname)
+    })
+  }
+}).then(channelname => {
+  // constructor (opts = { stdin: null, stdout: null, prompt: null, onUserInput: null }) {
+  const username = process.env.USERNAME
+  const password = process.env.TOKEN
+  new Chat({ channelname, username, password, terminal }).start()
+}).catch(e => {
+  console.error(e)
+  process.exit(1)
 })
-
-chat.onConnect(() => {
-  terminal.onUserInput(msg => chat.say(msg))
-  terminal.start(`${chat.getUsername()}: `)
-})
-
-terminal.setup(process.stdin, process.stdout)
-if (process.env.CHANNEL) {
-  start(process.env.CHANNEL)
-} else {
-  terminal.question('What channel do you want to join? ', start)
-}
